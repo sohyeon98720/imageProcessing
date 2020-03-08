e97f5ea3c2f4ceb0ce229ba3cf37f4aa7681673c
sohyeon98720 <47767202+sohyeon98720@users.noreply.github.com>
2020-03-08 21:40:02 +0900

    Add files via upload

---
+++ src.cpp
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <Windows.h>
+#include <iostream>
+#define SIZE 3
+using namespace std;
+
+void Binarization(BYTE * In, BYTE * Out, int Th, int W, int H)
+{
+	int Size = W * H;
+	for (int i = 0; i < Size; i++)
+	{
+		if(In[i] > Th) Out[i] = 255;
+		else Out[i] = 0;
+	}
+}
+
+int push(short *stackx, short *stacky, int arr_size, short vx, short vy, int *top)
+{
+	if (*top >= arr_size) return(-1);
+	(*top)++;
+	stackx[*top] = vx;
+	stacky[*top] = vy;
+	return(1);
+}
+
+int pop(short *stackx, short *stacky, short *vx, short *vy, int *top)
+{
+	if (*top == 0) return(-1);
+	*vx = stackx[*top];
+	*vy = stacky[*top];
+	(*top)--;
+	return(1);
+}
+// GlassFire 알고리즘을 이용한 라벨링 함수
+void m_BlobColoring(BYTE* CutImage, int height, int width)
+{
+	int i, j, m, n, top, area, Out_Area, index, BlobArea[1000];
+	long k;
+	short curColor = 0, r, c;
+	//	BYTE** CutImage2;
+	Out_Area = 1;
+
+
+	// 스택으로 사용할 메모리 할당
+	short* stackx = new short[height*width];
+	short* stacky = new short[height*width];
+	short* coloring = new short[height*width];
+
+	int arr_size = height * width;
+
+	// 라벨링된 픽셀을 저장하기 위해 메모리 할당
+
+	for (k = 0; k<height*width; k++) coloring[k] = 0;  // 메모리 초기화
+
+	for (i = 0; i<height; i++)
+	{
+		index = i*width;
+		for (j = 0; j<width; j++)
+		{
+			// 이미 방문한 점이거나 픽셀값이 255가 아니라면 처리 안함
+			if (coloring[index + j] != 0 || CutImage[index + j] != 255) continue;
+			r = i; c = j; top = 0; area = 1;
+			curColor++;
+
+			while (1)
+			{
+			GRASSFIRE:
+				for (m = r - 1; m <= r + 1; m++)
+				{
+					index = m*width;
+					for (n = c - 1; n <= c + 1; n++)
+					{
+						//관심 픽셀이 영상경계를 벗어나면 처리 안함
+						if (m<0 || m >= height || n<0 || n >= width) continue;
+
+						if ((int)CutImage[index + n] == 255 && coloring[index + n] == 0)
+						{
+							coloring[index + n] = curColor; // 현재 라벨로 마크
+							if (push(stackx, stacky, arr_size, (short)m, (short)n, &top) == -1) continue;
+							r = m; c = n; area++;
+							goto GRASSFIRE;
+						}
+					}
+				}
+				if (pop(stackx, stacky, &r, &c, &top) == -1) break;
+			}
+			if (curColor<1000) BlobArea[curColor] = area;
+		}
+	}
+
+	float grayGap = 255.0f / (float)curColor;
+
+	// 가장 면적이 넓은 영역을 찾아내기 위함 
+	for(i=1; i<=curColor; i++)
+	{
+		if(BlobArea[i]>=BlobArea[Out_Area]) Out_Area = i;
+	}
+	// CutImage 배열 클리어~
+	for (k = 0; k < width*height; k++) CutImage[k] = 255;
+
+	// coloring에 저장된 라벨링 결과중 (Out_Area에 저장된) 영역이 가장 큰 것만 CutImage에 저장
+	for (k = 0; k < width*height; k++)
+	{
+		if(coloring[k] == Out_Area) CutImage[k] = 0;  // 가장 큰 것만 저장
+		//CutImage[k] = (unsigned char)(coloring[k] * grayGap);
+	}
+
+	delete[] coloring;
+	delete[] stackx;
+	delete[] stacky;
+}
+// 라벨링 후 가장 넓은 영역에 대해서만 뽑아내는 코드 포함
+void findCenter(BYTE*Input,BYTE*Output,int W, int H) {
+	int x = 0;
+	int y = 0;
+	int count = 0;
+	for (int i = 0;i < H - 1;i++) {
+		for (int j = 0;j <  W- 1;j++) {
+			if (Output[(i*W) + j] == 0) {
+				x += j;
+				y += i;
+				count++;
+			}
+		}
+	}//동공부분에서 x,y좌표 구하기
+	x = x / count;
+	y = y / count;//무게중심 구하기
+	cout << "x좌표는" << x << endl;
+	cout << "y좌표는" << y << endl;
+
+	for (int i = 0;i < H - 1;i++) {
+		for (int j = 0;j < W - 1;j++) {
+			if (j == x || i == y) {
+				Input[(i*W) + j] = 255;
+			}
+		}
+	}//무게중심 x,y를 기준으로 선그리기
+}
+
+void main()
+{
+	BITMAPFILEHEADER hf; // BMP 파일헤더 14Bytes
+	BITMAPINFOHEADER hInfo; // BMP 인포헤더 40Bytes
+	RGBQUAD hRGB[256]; // 팔레트 (256 * 4Bytes)
+	FILE *fp;
+	fp = fopen("pupil2.bmp", "rb");
+	if (fp == NULL) return;
+	fread(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
+	fread(&hInfo, sizeof(BITMAPINFOHEADER), 1, fp);
+	fread(hRGB, sizeof(RGBQUAD), 256, fp);
+	int ImgSize = hInfo.biWidth * hInfo.biHeight;
+	BYTE * Image = (BYTE *)malloc(ImgSize);
+	BYTE * Output = (BYTE *)malloc(ImgSize);
+	fread(Image, sizeof(BYTE), ImgSize, fp);
+	fclose(fp);
+	int W = hInfo.biWidth, H = hInfo.biHeight;
+
+	/* 영상처리 */
+	Binarization(Image, Output, 50, W, H);
+	for (int i = 0; i < ImgSize; i++) Output[i] = 255 - Output[i];
+	m_BlobColoring(Output, H, W);
+	findCenter(Image,Output,W,H);
+	/* 영상처리 */
+
+	fp = fopen("output_pupil2.bmp", "wb");
+	fwrite(&hf, sizeof(BYTE), sizeof(BITMAPFILEHEADER), fp);
+	fwrite(&hInfo, sizeof(BYTE), sizeof(BITMAPINFOHEADER), fp);
+	fwrite(hRGB, sizeof(RGBQUAD), 256, fp);
+	fwrite(Image, sizeof(BYTE), ImgSize, fp);
+	fclose(fp);
+	free(Image);
+	free(Output);
+}
